6e2e3775daeb368ed5fd0935064bd138f813503d
taha0099-dev <tahasubhan233@gmail.com>
2026-09-26 00:47:33 +0500

    Add grid of colored boxes to main.dart
    Added a body with colored boxes arranged in rows.

--- main.dart
+++ main.dart
@@ -19,3 +19,57 @@ class Myapp extends StatelessWidget {
     );
   }
 }
+  body: Center(
+          child: Column(
+            children: [
+              Row(
+                children: [
+                  box(Colors.red),
+                  box(Colors.white),
+                  box(Colors.white),
+                  box(Colors.blue),
+                ],
+              ), // Row
+              Row(
+                children: [
+                  box(Colors.white),
+                  box(Colors.green),
+                  box(Colors.yellow),
+                  box(Colors.white),
+                ],
+              ), // Row
+              Row(
+                children: [
+                  box(Colors.white),
+                  box(Colors.yellow),
+                  box(Colors.green),
+                  box(Colors.white),
+                ],
+              ), // Row
+              Row(
+                children: [
+                  box(Colors.blue),
+                  box(Colors.white),
+                  box(Colors.white),
+                  box(Colors.red),
+                ],
+              ), // Row
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget box(Color color) {
+  return Container(
+    width: 50,
+    height: 50,
+    margin: const EdgeInsets.all(1),
+    decoration: BoxDecoration(
+      color: color,
+      border: Border.all(color: Colors.black, width: 1),
+    ),
+  );
+}
