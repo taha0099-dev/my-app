34ce74638d1c18118977bdf8d95d9940cb2e5378
taha0099-dev <tahasubhan233@gmail.com>
2026-09-26 00:59:07 +0500

    Refactor app to display a chess board
    Updated the main widget to display a chess board layout with alternating colors.

--- main.dart
+++ main.dart
@@ -1,4 +1,8 @@
+LOUDO------------------------------------
+
 import 'package:flutter/material.dart';
+
+
 
 void main() {
   runApp(const Myapp());
@@ -70,6 +74,128 @@ Widget box(Color color) {
     decoration: BoxDecoration(
       color: color,
       border: Border.all(color: Colors.black, width: 1),
+    ),
+  );
+}
+
+CHESS------------------------------
+
+  import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Chess Board'),
+          backgroundColor: const Color.fromARGB(255, 173, 200, 0),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              Row(children: [
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+              ]), // Row 1
+              Row(children: [
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+              ]), // Row 2
+              Row(children: [
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+              ]), // Row 3
+              Row(children: [
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+              ]), // Row 4
+              Row(children: [
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+              ]), // Row 5
+              Row(children: [
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+              ]), // Row 6
+              Row(children: [
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+              ]), // Row 7
+              Row(children: [
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+                box(Colors.black),
+                box(Colors.white),
+              ]), // Row 8
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+// Simple helper function: give it a color, it gives you a box
+Widget box(Color color) {
+  return Container(
+    width: 40,
+    height: 40,
+    decoration: BoxDecoration(
+      color: color,
+      border: Border.all(color: Colors.grey, width: 1),
     ),
   );
 }
